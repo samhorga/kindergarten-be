9c0cbafa8f2fbd0967856aebb806c6bab3881f4b
samuelhorga <samyhorga@yahoo.com>
2023-11-11 15:46:33 -0700

    first commit

---
+++ src/main/java/com/project/kindergartenbe/KindergartenBeApplication.java
@@ -0,0 +1,13 @@
+package com.project.kindergartenbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindergartenBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KindergartenBeApplication.class, args);
+    }
+
+}
